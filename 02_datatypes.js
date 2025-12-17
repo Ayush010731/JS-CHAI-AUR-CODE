f0671dd8696a312c6472a70e2fc36fbcb786f79b
Ayush010731 <ayushojha072004@gmail.com>
2025-12-17 22:29:51 +0530

    Initial commit - JS Chai Aur Code

---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3) //we are using nodejs not browser hence it will give error
+
+ let name = "ayush"
+ let age = 20 
+ let isloggedIN = true
+
+ //JS datatypes
+ // number => 2 to power 53
+ //bigint 
+ // string=> ""
+ // boolean => true/false
+ //null
+ //undefined =>
+// symbol => used in react and are unique
+
+ //object 
+
+ console.log(typeof ayush); //result is string
+ console.log(typeof undefined); // result is undefined
+ console.log(typeof null); //result is object
