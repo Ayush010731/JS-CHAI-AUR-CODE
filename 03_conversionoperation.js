ba77972d55ddef6df9d82045702cf88ae56685c2
Ayush010731 <ayushojha072004@gmail.com>
2025-12-17 22:57:14 +0530

    Added & 'c:\Users\AYUSH\OneDrive\Desktop\JS CHAI AUR CODE\03_conversionoperation.js'

---
+++ 03_conversionoperation.js
@@ -0,0 +1,10 @@
+let score = "33" 
+console.log(typeof score) ; // it will give string
+let valueInNumber = Number(score ) // its is use to convert into number
+
+// "33" => 33
+ // "33abc" => return NaN
+ // true => return 1 as boolean value
+
+ // booleanIsLoggedIn will covert 1 = true ; 0 = false
+ // "" = false  //"Ayush" = true
