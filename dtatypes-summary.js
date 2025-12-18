cbe62a44dd3ea5a1823edaf8e1c124ab0aabd29f
Ayush010731 <ayushojha072004@gmail.com>
2025-12-18 22:37:28 +0530

    data type summary

---
+++ dtatypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive 
+
+// 7 types : string  , number , boolean , null , undefined , Symbol , BigInt
+
+
+// Reference data type(Non primitive)
+
+// Array  , objects , Functions
+// example of array 
+const heros =["shaktiman" , "naagraj"]
+
+// example of object 
+{
+    name : "Ayush"
+    age : 21 
+}
+// example of functions 
+   const myFunction = function(){
+   console.log("HELLO WORLD");
+   }
