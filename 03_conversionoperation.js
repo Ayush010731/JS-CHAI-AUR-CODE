1039205cc5e288b8d0aead02de0d20e1a8b39068
Ayush010731 <ayushojha072004@gmail.com>
2025-12-18 22:13:04 +0530

    conversion operations

--- 03_conversionoperation.js
+++ 03_conversionoperation.js
@@ -8,3 +8,17 @@ let valueInNumber = Number(score ) // its is use to convert into number
 
  // booleanIsLoggedIn will covert 1 = true ; 0 = false
  // "" = false  //"Ayush" = true
+
+ // ****************operations*********
+ let value = 3 
+ let negValue = - value
+ console.log(negValue) // It will return negative value
+
+ //console.log(2+2)
+ //console.log(2-2)
+ //console.log(2*2)
+ //console.log(2**2)
+
+ let gameCounter = 100
+ gameCounter++ 
+ console.log(gameCounter) // it will give 101 output
