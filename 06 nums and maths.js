7223d6232f8fa8f77ab3a0a156a4481d1fb2710e
Ayush010731 <ayushojha072004@gmail.com>
2025-12-20 22:41:22 +0530

    maths and number

---
+++ 06 nums and maths.js
@@ -0,0 +1,15 @@
+const score = 400 // it is use to define numbers   
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4)) // it will give output of 4 digit only ie 123.9
+
+// ++++++++++++++ Maths ++++++++++++++
+
+console.log(Math.abs(4)) ; // result is 4
+console.log(Math.round(4.6)); // reuslt is 5 
+console.log(Math.ceil(4.6)) ; // reuslt is 5 if even one value is also greater then 4 then it is 5
+console.log(Math.floor(4.6)) ; // reuslt is 4 it gives lower value
+
+console.log(Math.random()); // reuslt will be any value between 0 and 1
+
