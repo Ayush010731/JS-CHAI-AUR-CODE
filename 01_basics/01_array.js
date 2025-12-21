c06f6b03391a875b906402b77abf4ab4384669a9
Ayush010731 <ayushojha072004@gmail.com>
2025-12-21 22:11:33 +0530

    array in js

---
+++ 01_basics/01_array.js
@@ -0,0 +1,14 @@
+// array
+
+const myArr = [0,1,2,3,4,5]
+
+console.log(myArr[2]) //use to access element in the array
+
+// Array methods
+
+myArr.push(6); // it will push 6 in the array
+myArr.pop(6); // it will pop last element of array
+
+myArr.unshift(7); // it will insert 7 at the start of array
+
+
