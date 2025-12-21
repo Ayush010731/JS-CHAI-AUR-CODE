287514bf470d55486ad5619650167c58121153dc
Ayush010731 <ayushojha072004@gmail.com>
2025-12-21 22:25:58 +0530

    array part 2

---
+++ 01_basics/02_array.js
@@ -0,0 +1,13 @@
+const marvel_heros = ["thor" , "Ironman" , "spiderman"]
+const dc = ["superman" , "flash" , "batman"]
+
+marvel_heros.push(dc) // it will form array in array by considering dc as element
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+let score4 = 400 
+
+console.log(Array.of(score1 , score2 , score3 , score4)); // it will form a array of all the values 
+
+
