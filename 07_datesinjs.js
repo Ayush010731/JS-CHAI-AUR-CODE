01be9d3b72ef768c297f2be338088e7adde6d8d0
Ayush010731 <ayushojha072004@gmail.com>
2025-12-21 21:37:24 +0530

    date and time

---
+++ 07_datesinjs.js
@@ -0,0 +1,12 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate);//it will give date
+
+console.log(myDate.toString()); // it will convert date in string
+
+console.log(typeof myDate); // it will give result as object
+
+let myCreatedDate = new Date(2023 , 0 , 23)
+console.log(myCreatedDate.toDateString()); // it will convert personalised date into string
+
