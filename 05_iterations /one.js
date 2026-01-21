dfb0c6d5d04bb37d6c6d76ae5126ca30ea96ca86
Ayush010731 <ayushojha072004@gmail.com>
2026-01-21 14:08:49 +0000

    for loop

---
+++ 05_iterations /one.js
@@ -0,0 +1,16 @@
+// for 
+
+for (let index = 0; index < 5; index++) {
+    console.log('Iteration number:', index);
+    }
+  // for loop to iterate from 0 to 4 and log the iteration number
+//   // Output:
+//   Iteration number: 0
+//  Iteration number: 1
+//   Iteration number: 2
+//   Iteration number: 3
+//   Iteration number: 4
+ //for loop example
+ for (let i = 0; i < 5; i++) {
+    console.log('Iteration number:', i);
+}
