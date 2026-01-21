75f40a282c299cc5ae5e45a77fc29a9f52a5cbb1
Ayush010731 <ayushojha072004@gmail.com>
2026-01-21 14:52:46 +0000

    map

---
+++ 05_iterations /three.js
@@ -0,0 +1,15 @@
+// forof loop 
+const array = ['a', 'b', 'c', 'd', 'e'];
+for (const element of array) {
+    console.log('Element:', element);
+}
+
+// Maps 
+const map = new Map([
+    ['key1', 'value1'],
+    ['key2', 'value2'],
+    ['key3', 'value3']
+]);
+for (const [key, value] of map) {
+    console.log(`Key: ${key}, Value: ${value}`);
+}
