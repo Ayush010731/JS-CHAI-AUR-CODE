5d839046f48c194caf998b413ca3861cc867cfba
Ayush010731 <ayushojha072004@gmail.com>
2026-01-21 14:22:02 +0000

    while loop

---
+++ 05_iterations /two.js
@@ -0,0 +1,13 @@
+// while loop to iterate from 0 to 4 and log the iteration number
+let count = 0;
+while (count < 5) {
+    console.log('Iteration number:', count);
+    count++;
+}
+// do...while loop to iterate from 0 to 4 and log the iteration number
+let num = 0;
+do {
+    console.log('Iteration number:', num);
+    num++;
+} while (num < 5);
+console.log('Done with iterations!');
